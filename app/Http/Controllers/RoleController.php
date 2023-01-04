570cf78e54e4b473e6697ebfd15fd9258552c54e
Ezequiel Rodriguez <ezekieru333@gmail.com>
2023-01-03 22:03:34 -0300

    first commit

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
+
+class RoleController extends Controller
+{
+    function __construct()
+    {
+         $this->middleware('permission:read-rol|create-rol|update-rol|destroy-rol', ['only' => ['index']]);
+         $this->middleware('permission:create-rol', ['only' => ['create','store']]);
+         $this->middleware('permission:update-rol', ['only' => ['edit','update']]);
+         $this->middleware('permission:destroy-rol', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::paginate(5);
+        return Inertia::render('roles.index', ['roles' => $roles]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permission = Permission::get();
+        return Inertia::render('roles.create', ['permission' => $permission]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required',
+        ]);
+    
+        $role = Role::create(['name' => $request->name]);
+        $role->syncPermissions($request->permission);
+    
+        return redirect()->route('roles.index');   
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $role = Role::find($id);
+        $permission = Permission::get();
+        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+            ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
+            ->all();
+    
+        return Inertia::render('roles.edit',compact('role','permission','rolePermissions'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'permission' => 'required',
+        ]);
+    
+        $role = Role::find($id);
+        $role->name = $request->name;
+        $role->save();
+    
+        $role->syncPermissions($request->permission);
+    
+        return redirect()->route('roles.index');         
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table("roles")->where('id',$id)->delete();
+        return redirect()->route('roles.index');      
+    }
+}
